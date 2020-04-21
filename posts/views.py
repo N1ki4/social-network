0c0817e6ce10d6d8d0b911057e6a7850dda10121
n1ki4 <glazunov_n@ukr.net>
2020-04-21 15:53:14 +0300

    added models, endpoint for creating post

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Post
+from .serializers import PostSerializer
+
+
+class PostList(APIView):
+    serializer_class = PostSerializer
+    def post(self, request):
+        serizlier = PostSerializer(data=request.data)
+        if serizlier.is_valid():
+            serizlier.save()
+            return Response(serizlier.data, status=status.HTTP_201_CREATED)
+        return Response(serizlier.errors, status=status.HTTP_400_BAD_REQUEST)
